c1ef5fc645fa71a810482d86a881ed22584deac5
Jani-Music <hahahsygs395@gmail.com>
2025-09-10 01:58:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_HASH = "3bcc137f124bc8518c30d461986e247b"
 BOT_TOKEN = getenv("BOT_TOKEN", "7940019678:AAFnZD4f0dM_PPZm9vbmuv_HGPxw6EjGswo")
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://oliva:oliva123@cluster0.6cohqfv.mongodb.net/?retryWrites=true&w=majority")
-OWNER_ID = int(getenv("OWNER_ID", "6693611573"))
-SUPPORT_GRP = "Jani_RP"
-UPDATE_CHNL = "Jani_RP"
-OWNER_USERNAME = "Jani_RP_Lover"
+OWNER_ID = int(getenv("OWNER_ID", "7666408563"))
+SUPPORT_GRP = "Mehu_420"
+UPDATE_CHNL = "Mehu_420"
+OWNER_USERNAME = "Mehu_420"
